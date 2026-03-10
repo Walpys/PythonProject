ba006b541c80160572911b3a61432c88b63cdf56
Ivan Hudko <giv.10072007@gmail.com>
2026-03-10 13:45:42 +0200

    create .venv and requirements.txt

--- views/main_ui.py
+++ views/main_ui.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\ВНУ\Спеціалізовані мови програмування\Job_agregator\views\main.ui'
+# Form implementation generated from reading ui file 'd:\ВНУ\Спеціалізовані мови програмування\PythonProject\views\main.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.11
 #
